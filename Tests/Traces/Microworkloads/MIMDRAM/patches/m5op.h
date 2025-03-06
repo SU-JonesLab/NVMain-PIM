cffc1a82e280576c8bd543082a854cf879a903f3
Benjamin Morris <bfm16@hl279-cmp-04.egr.duke.edu>
2025-03-05 19:09:45 -0500

    tests reorganizes and mimdram patches added

---
+++ Tests/Traces/Microworkloads/MIMDRAM/patches/m5op.h
@@ -0,0 +1,7 @@
+#ifndef M5OP_H
+#define M5OP_H
+
+static inline void m5_reset_stats(int a, int b) { /* no-op */ }
+static inline void m5_dump_stats(int a, int b)  { /* no-op */ }
+
+#endif // M5OP_H
